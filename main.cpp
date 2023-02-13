50bb21d18508d0b7df4a96f9a5ae32dc47864f39
Igor <denisovigor98@mail.ru>
2023-02-13 14:10:27 +0300

    Test_Visual_code

--- main.cpp
+++ main.cpp
@@ -6,85 +6,46 @@
 #include <map>
 #include <functional>
 #include <utility> 
-#include "../../../../../APSTEC/c++/json-develop/single_include/nlohmann/json.hpp"
+
  
 
 std::string pig_it(std::string str)
 {
     std::string temp;
     std::string res;
-   // char * i = &str[0]; 
+
 
     for (int i = 0; i < str.size(); ++i)
     {
-        if (str[i] == '!' || str[i] == '?' || str[i] == ',' || str[i] == ':' || str[i] == ' ' || i == str.size() - 1)
+        if (str[i] == '!' || str[i] == '?' || str[i] == ',' || str[i] == ':')
         {
             res += str[i];
+            continue;
         }
-        else
+        if (str[i] == ' ' || i == str.size() - 1)
         {
-            temp += str[i];
+            if (i == str.size() - 1)
+            {
+                 temp += str[i];
+            }
+           
+            for (int j = 1; j < temp.size(); ++j)
+            {
+                res += temp[j];
+            }
+            res += temp[0];
+            res += "ay ";
+            temp.clear();
             continue;
         }
 
-        for (int j = 1; j < temp.size(); ++j)
-        {
-            res += temp[j];
-        }
-        res += temp[0]; 
-        res += "ay ";
-        temp.clear();
-        // if ()
-        // {
-            // if (i == str.size() - 1)
-            // {
-            //      temp += str[i];
-            // }
-           
-            
-            // continue;
-        // }
-        // temp += str[i];
+        temp += str[i];
     }
-    // if (res.size() > 0)
-    // {
-    //     res.pop_back();
-    // }
+    if (res.size() > 0)
+    {
+        res.pop_back();
+    }
     return res;
-//     std::string temp;
-//     std::string res;
-//    // char * i = &str[0]; 
-
-//     for (int i = 0; i < str.size(); ++i)
-//     {
-//         if (str[i] == '!' || str[i] == '?' || str[i] == ',' || str[i] == ':')
-//         {
-//             res += str[i];
-//             continue;
-//         }
-//         if (str[i] == ' ' || i == str.size() - 1)
-//         {
-//             if (i == str.size() - 1)
-//             {
-//                  temp += str[i];
-//             }
-           
-//             for (int j = 1; j < temp.size(); ++j)
-//             {
-//                 res += temp[j];
-//             }
-//             res += temp[0];
-//             res += "ay ";
-//             temp.clear();
-//             continue;
-//         }
-//         temp += str[i];
-//     }
-//     if (res.size() > 0)
-//     {
-//         res.pop_back();
-//     }
-//     return res;
 }
 
 int main()
